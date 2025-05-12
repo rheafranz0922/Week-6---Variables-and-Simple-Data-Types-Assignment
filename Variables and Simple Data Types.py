291dd4722450e584bbd52f854310eabd6521c544
rheafranz0922 <114682727+rheafranz0922@users.noreply.github.com>
2025-05-13 01:49:55 +0400

    Create Variables and Simple Data Types.py
    hi

---
+++ Variables and Simple Data Types.py
@@ -0,0 +1,32 @@
+# Put first name and last names.
+first_name = "rhea"
+LAST_NAME = "JUANIZA"
+
+#Print the hello message to string the manipulation
+print(f"Hello, {first_name.upper()} {LAST_NAME.lower()}\n")
+
+# Print Quotes
+print("\n\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible - Francis of Assisi\"")
+
+# Decimal Numbers
+num1 = 8.75
+num2 = 3.2
+
+# Arithmetic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# print results
+print(f"\n{num1} plus {num2} equals {addition}")
+print(f"{num1} minus {num2} equals {subtraction}")
+print(f"{num1} times {num2} equals {multiplication}")
+print(f"{num1} divided by  {num2} equals {division}")
+
+# Current month
+month = "May"
+day_of_month = 12
+
+#Print format
+print(f"\n\t\tToday is day {day_of_month} of the month of {month}.")
